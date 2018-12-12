4d33b6a0b90bab320d7f13fd4db381aa31d5697b
nachoespino53 <nachoespino53@gmail.com>
2018-12-12 12:36:29 -0500

    Auth feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   resources :items, only: [:index, :create, :update, :destroy]
   resources :orders, only: [:index, :create, :update, :destroy]
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end
 end
