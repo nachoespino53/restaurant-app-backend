293d0533adeb3f78b3bd1d445d5eaa6a5b3ad03d
nachoespino53 <nachoespino53@gmail.com>
2018-12-20 11:29:32 -0500

    Database bug fix

--- db/migrate/20181220162359_create_ingredients.rb
+++
@@ -1,8 +0,0 @@
-class CreateIngredients < ActiveRecord::Migration[5.2]
-  def change
-    create_table :ingredients do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20181220162456_create_item_ingredients.rb
+++
@@ -1,8 +0,0 @@
-class CreateItemIngredients < ActiveRecord::Migration[5.2]
-  def change
-    create_table :item_ingredients do |t|
-
-      t.timestamps
-    end
-  end
-end
