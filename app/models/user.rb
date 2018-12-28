846031e28396d9093d387fca613b6c4550af9df6
nachoespino53 <nachoespino53@gmail.com>
2018-12-28 17:38:38 -0500

    Validations for creating a new User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false}
+    validates :email, uniqueness: true
+    validates :password, length: { in: 6..20 }
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :phone_number, length: { is: 10}
+    validates :phone_number, numericality: { only_integer: true }
     # attr_accessor :image_url
     mount_uploader :image_url, ImageUploader
 end
